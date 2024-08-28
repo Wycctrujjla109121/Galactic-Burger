6ad9fa00b2454f5b0a76ce6384ddaf0242e74dd5
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-08-28 20:30:53 +0300

    fix: добавил ModalOverlay и createPortal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,8 @@
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import s from './modal.module.scss'
 import { useEffect } from 'react';
+import { ModalOverlay } from '../modal-overlay';
+import { createPortal } from 'react-dom';
 
 export const Modal = ({
     withTitle,
@@ -27,9 +29,9 @@ export const Modal = ({
         };
     })
 
-    return (
-        <div onClick={setIsOpen} className={[s.wrapper, isOpen ? s.wrapper_open : s.wrapper_close].join(' ')}>
-            <div onClick={e => e.stopPropagation()} className={s.wrapper__info}>
+    return createPortal(
+        <div className={[s.wrapper, isOpen ? s.wrapper_open : s.wrapper_close].join(' ')}>
+            <div className={s.wrapper__info}>
                 <div className={`pt-15 pr-10 pl-10 ${s.wrapper__header}`}>
                     <p className="text text_type_main-large">
                         {withTitle ? 'Детали ингридиента' : ''}
@@ -40,6 +42,8 @@ export const Modal = ({
                 </div>
                 {children}
             </div>
-        </div>
-    );
+
+            <ModalOverlay setIsOpen={setIsOpen} />
+        </div>, document.body
+    )
 };
