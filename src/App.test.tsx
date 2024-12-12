f6fd9a02898de52cfdf094a8db4bd99cfe0ba232
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-12-12 19:03:41 +0300

    откат изменений cypress, ломает билд

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Ломается почему-то
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
